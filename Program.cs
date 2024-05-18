4511bc0e674f1b6c60dd632b586316ffc94c0858
Zaid Zuhair <zaidzuhairalani@gmail.com>
2024-05-18 18:16:16 +0300

    Add Stock Controller

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<ApplicationDBContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
