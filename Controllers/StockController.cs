ab0a5e16058d3d10fe150dcb56ca0687763c6e0b
Zaid Zuhair <zaidzuhairalani@gmail.com>
2024-05-21 14:59:53 +0300

    Make the first DELETE req for Stock

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -73,5 +73,23 @@ namespace api.Controllers
 
             return Ok(stockModel.ToStockDto());
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stock.FirstOrDefault(x => x.Id == id);
+
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stock.Remove(stockModel);
+            
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
