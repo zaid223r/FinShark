fe0bacc4e1aacd5c89d1ba0dc1b58cbb9b632d73
Zaid Zuhair <zaidzuhairalani@gmail.com>
2024-05-21 14:54:32 +0300

    Make the first PUT req for Stock

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -7,6 +7,7 @@ using api.Mappers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using api.Dtos;
+using api.Dtos.Stock;
 
 namespace api.Controllers
 {
@@ -48,6 +49,29 @@ namespace api.Controllers
             _context.Stock.Add(stockModel);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new {id = stockModel.Id}, stockModel.ToStockDto());            
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id,[FromBody] UpdateStockRequestDto updateStockRequestDto)
+        {
+            var stockModel = _context.Stock.FirstOrDefault(x => x.Id == id);
+
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateStockRequestDto.Symbol;
+            stockModel.CompanyName = updateStockRequestDto.CompanyName;
+            stockModel.Purchase = updateStockRequestDto.Purchase;
+            stockModel.LastDiv = updateStockRequestDto.LastDiv;
+            stockModel.Industry = updateStockRequestDto.Industry;
+            stockModel.MarketCap = updateStockRequestDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
         }
     }
 }
