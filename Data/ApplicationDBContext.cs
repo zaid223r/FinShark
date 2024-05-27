cb394817d4f1c5ea6894e7436575800e21ecd598
Zaid Zuhair <zaidzuhairalani@gmail.com>
2024-05-27 16:24:54 +0300

    Make the registration work

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using api.Migrations;
 using api.Models;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +21,24 @@ namespace api.Data
 
         public DbSet<Stock> Stock { get; set;} //make Stock table
         public DbSet<Comment> Comments { get; set;} //make Comments table
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
 
+            List<IdentityRole> roles= new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
